b6e6c7f109116fa0c600a295db8fe761527ea19a
黄海林 <10000@cxzfb.cn>
2018-09-06 18:20:01 +0800

    支持自动选中侧边栏，调整了margin高度

--- _assets/hm.js
+++ _assets/hm.js
@@ -4,6 +4,9 @@ $(function (){
     $('.header.sticky').sticky({
         context: '#page'
     });
+    var activeContent = $('.ui.accordion .content .item.active').parent('.content');
+    activeContent.addClass('active');
+    activeContent.prev('.title').addClass('active');
     // $('.slidenav.sticky').sticky({
     //     offset: 120,
     //     context: '.container'
